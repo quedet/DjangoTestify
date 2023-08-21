acab3ae98bead0aa68a7423ef542903b271cfc25
fidelquedet <fidelquedet10@gmail.com>
2023-08-21 20:06:30 +0000

    Todo Views + Urls

--- DjangoTestify/storages_backends.py
+++ DjangoTestify/storages_backends.py
@@ -3,6 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
+    file_overwrite = True
 
 
 class PublicMediaStorage(S3Boto3Storage):
