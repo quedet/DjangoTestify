6b0f185ee91a2aaaae08d212c429cdb67eae4b01
fidelquedet <fidelquedet10@gmail.com>
2023-08-21 19:06:33 +0000

    Added Crispy For Forms Display

--- DjangoTestify/settings.py
+++ DjangoTestify/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'storages',
     'webpack_boilerplate'
+    'crispy_forms',
+    'crispy_tailwind'
 ]
 
 MIDDLEWARE = [
@@ -114,9 +116,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Crispy Forms
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 WEBPACK_LOADER = {
     'MANIFEST_FILE': os.path.join(BASE_DIR, 'frontend/build/manifest.json')
 }
