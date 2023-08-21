903bbc65c4c5c5c692759664f081e46ce7f1c851
fidelquedet <fidelquedet10@gmail.com>
2023-08-21 19:12:09 +0000

    Todo Model

--- apps/todos/models.py
+++ apps/todos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    content = models.CharField(max_length=200, unique=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
